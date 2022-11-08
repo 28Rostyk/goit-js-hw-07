fadd0d36b819316ebbe4c6f9794ac17c23f65acd
28Rostyk <107403659+28Rostyk@users.noreply.github.com>
2022-11-08 17:52:48 +0200

    homework-7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,79 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const refs = {
+  galleryContainer: document.querySelector(".gallery"),
+  body: document.body,
+};
 
-console.log(galleryItems);
+const galleryMarkup = createImgGalleryMarkup(galleryItems);
+function createImgGalleryMarkup(item) {
+  return item
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+     <img loading="lazy" width="354" height="240"
+       class="gallery__image"
+       src="${preview}"
+       data-source="${original}"
+      alt="${description}"
+     />
+   </a>
+ </div>`;
+    })
+    .join("");
+}
+refs.galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+function createLightbox(imageAdress) {
+  window.instance = basicLightbox.create(
+    `
+    <img src="${imageAdress}">
+`,
+    {
+      onShow: () => window.addEventListener("keydown", closeModalWindow),
+      onClose: () => {
+        window.removeEventListener("keydown", closeModalWindow);
+        refs.body.classList.remove("disable-scroll");
+      },
+    }
+  );
+  return instance;
+}
+
+refs.galleryContainer.addEventListener("click", onClickImgOpenModal);
+
+function onClickImgOpenModal(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+  const originalImageRef = event.target.dataset.source;
+  createLightbox(originalImageRef).show();
+  refs.body.classList.add("disable-scroll");
+}
+
+function closeModalWindow(event) {
+  const ESC_KEY_CODE = "Escape";
+  if (event.code === ESC_KEY_CODE && instance.visible()) {
+    instance.close();
+    refs.body.classList.remove("disable-scroll");
+  }
+}
+
+const lazyImages = refs.galleryContainer.querySelectorAll(".gallery__image");
+
+lazyImages.forEach((image) =>
+  image.addEventListener("load", onImageLoaded, { once: true })
+);
+
+function onImageLoaded(event) {
+  event.target.classList.add("appear");
+}
+lazyImages.forEach((image) =>
+  image.addEventListener("mouseenter", onMouseEnter)
+);
+
+function onMouseEnter(event) {
+  event.target.style.transitionDelay = "100ms";
+  event.target.style.transitionDuration = "500ms";
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,50 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
+import { addGalleryItems } from "./addGallery-items.js";
 
-console.log(galleryItems);
+// Change code below this line
+const newGalleryItems = [...galleryItems, ...addGalleryItems];
+console.log(newGalleryItems);
+const refs = {
+  galleryContainer: document.querySelector(".gallery"),
+  body: document.body,
+};
+
+const galleryMarkup = createImgGalleryMarkup(newGalleryItems);
+function createImgGalleryMarkup(item) {
+  return item
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item"><a class="gallery__link" href="${original}">
+     <img loading="lazy" width="354" height="240"
+       class="gallery__image"
+       src="${preview}"
+      alt="${description}"
+     />
+   </a></li>`;
+    })
+    .join("");
+}
+refs.galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  scrollZoom: false,
+});
+
+const lazyImages = refs.galleryContainer.querySelectorAll(".gallery__image");
+
+lazyImages.forEach((image) =>
+  image.addEventListener("load", onImageLoaded, { once: true })
+);
+
+function onImageLoaded(event) {
+  event.target.classList.add("appear");
+}
+lazyImages.forEach((image) =>
+  image.addEventListener("mouseenter", onMouseEnter)
+);
+
+function onMouseEnter(event) {
+  event.target.style.transitionDelay = "100ms";
+  event.target.style.transitionDuration = "500ms";
+}
